a56a6c2f48ec6144f4125781ad0e5087894307ec
Nochninog <vorotnikova.elizaveta@internet.ru>
2024-09-28 18:53:57 +0300

    make processing function

--- backend/static/main.js
+++ backend/static/main.js
@@ -43,20 +43,55 @@ function handleFiles() {
        
 }
 
-function handleSubmit(event){
+async function handleSubmit(event){
     event.preventDefault();
     const data = new FormData();
 
 
     data.append('file', file_1, file_1.name);
 
-    return fetch('/video', {
-        method: 'POST',
-        body: data,
-    });
+    try{
 
-    
+        let response = await fetch('/video', {
+            method: 'POST',
+            body: data,
+        });
+
+        if(response.ok){
+            let result = await response.json();
+            console.log(result);
+            let stage_1 = document.querySelector('#stage-1');
+            let stage_2 = document.querySelector('#stage-2');
+            stage_1.style.display = 'none';
+            stage_2.style.display = 'block';
+            processing(result.id);
+        }
+    }
+    catch (error) {
+        console.error('Ошибка при отправке данных на сервер:', error);
+    }    
+} 
+
+async function processing(id_video){
+    try{
+
+        let response = await fetch('/is_processing/' + id_video, {
+            method: 'GET',
+        });
+
+        if(response.ok){
+            let ready = response.json();
+            if(ready.processing === false){
+                console.log('Успех');
+            }
+            else{
+                setTimeout(() => processing(id_video), 1000)
+            }
+        }
+    }
+    catch (error) {
+        console.error('Ошибка при отправке данных на сервер:', error);
+    }    
 }
-
 myForm.addEventListener('submit', handleSubmit);
 
